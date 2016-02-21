4942c148e282c72b4b02f5e9cefb58d7cf604578
Boris Farber <bfarber@google.com>
2016-02-21 19:51:20 +0000

    command line arguments

--- dex-method-dumper/src/com/google/classyshark/silverghost/translator/dex/MethodDumper.kt
+++ dex-method-dumper/src/com/google/classyshark/silverghost/translator/dex/MethodDumper.kt
@@ -25,7 +25,7 @@ import java.util.zip.ZipInputStream
 class MethodDumper {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            println(MethodDumper().dumpMethods(File("/Users/bfarber/Desktop/Scenarios/4 APKs/com.android.chrome-52311111.apk")))
+            println(MethodDumper().dumpMethods(File(args[0])))
         }
     }
 
